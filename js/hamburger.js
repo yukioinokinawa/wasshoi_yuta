c2bbc6424d8d84ca08e8645a06fa7aef3d1c1110
Taka <yukioinokinawa@gmail.com>
2022-10-06 12:58:08 +0900

    フロントページ
    サイドバー未了

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,8 +1,18 @@
 jQuery(function ($) {
-    $(".js-hamburger").on("click", function () {
-        console.log("test");
-        $(this).toggleClass("is-open");
-        $(".c-layer--sidebar").toggleClass("is-open")
-        $(".l-sidebar").toggleClass("is-open");
+    if (window.matchMedia("(max-width: 1025px)").matches) {
+        $(".js-hamburger").on("click", function () {
+            $(this).toggleClass("is-open");
+
+            $(".c-layer--sidebar").toggleClass("is-open");
+            $(".l-sidebar").toggleClass("is-open");
+            $("body").toggleClass("is-open");
+            $(".p-sns").toggleClass("is-open");
+
+        } else {
+            $('.c-layer--sidebar').hide();
+            $('.l-sidebar').hide();
+        }
     });
-});
+
+
+
