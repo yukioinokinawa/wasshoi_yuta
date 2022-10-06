364ee04fb75817b205b513400b6296736de06ec7
kota machino <65933978+airraid714@users.noreply.github.com>
2022-10-07 00:26:57 +0900

    サイドバーの挙動、ナビゲーションのレスポンシブ対応まで

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,18 +1,13 @@
 jQuery(function ($) {
-    if (window.matchMedia("(max-width: 1025px)").matches) {
-        $(".js-hamburger").on("click", function () {
-            $(this).toggleClass("is-open");
 
-            $(".c-layer--sidebar").toggleClass("is-open");
-            $(".l-sidebar").toggleClass("is-open");
-            $("body").toggleClass("is-open");
-            $(".p-sns").toggleClass("is-open");
-
-        } else {
-            $('.c-layer--sidebar').hide();
-            $('.l-sidebar').hide();
-        }
+    $(".js-hamburger").on("click", function () {
+        $(this).toggleClass("is-open");
+        $(".c-layer--sidebar").toggleClass("is-open");
+        $(".l-sidebar").toggleClass("is-open");
+        $("body").toggleClass("is-open");
+       // $(".p-sns").toggleClass("is-open");
     });
+});
 
 
 
