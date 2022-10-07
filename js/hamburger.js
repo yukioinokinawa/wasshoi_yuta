9fc3e09b943953bdeac36d616f2cb7feefb66403
kota machino <65933978+airraid714@users.noreply.github.com>
2022-10-07 11:11:03 +0900

    jsファイルの修正,画面幅の本体修正
    ・ハンバガーメニューが出たままの状態で1025pxにレスポンシブさせるとハンバーガーメニューは消える。
    ・ハンバーガーメニューを出した際に画面幅が変わるのを解消させる。

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,11 +1,20 @@
 jQuery(function ($) {
 
     $(".js-hamburger").on("click", function () {
+        console.log("テスト");
         $(this).toggleClass("is-open");
         $(".c-layer--sidebar").toggleClass("is-open");
         $(".l-sidebar").toggleClass("is-open");
         $("body").toggleClass("is-open");
-       // $(".p-sns").toggleClass("is-open");
+    });
+
+    $(window).on('resize', function() {
+        if (window.innerWidth > 1025) {
+            $(".js-hamburger").removeClass("is-open");
+            $(".c-layer--sidebar").removeClass("is-open");
+            $(".l-sidebar").removeClass("is-open");
+            $("body").removeClass("is-open");    
+        }
     });
 });
 
